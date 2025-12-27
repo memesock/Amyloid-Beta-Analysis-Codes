6da3a665af378713fa0bab658561899bc3b9909a
memesock <memesock3@gmail.com>
2025-12-27 03:42:17 +0000

    Refactor START_SIMULATION.sh for clarity and updates

--- MARTINI3_SIMULATIONS/START_SIMULATION.sh
+++ MARTINI3_SIMULATIONS/START_SIMULATION.sh
@@ -1,31 +1,39 @@
 
 # ONE MUST HAVE AN ALL ATOMISTIC PDB STRUCTURE TO START THE SIMULATION.IN THIS SCRIPT, ITS DENOTED BY dimer.pdb
+# MORE INFORMATION ABOUT THERMOSTATS AND BAROSTATS IN THE MDP FOLDER FOR DIFFERENT STAGES OF RUN
 
+THIS NEXT SECTION IS THE MOST IMPORTANT IN DECIDING THE FORCE FIELD (MARTINI3, SCALED MARTINI3, MARTINI3-IDP
 #######################################################################
 ##############################################################
 ######################################################
 # FOR MARTINI 3 SIMULATION
-martinize2 -f dimer.pdb -o topol.top -x protein_cg.pdb -p backbone -ff martini3001 -cys auto -merge all
+martinize2 -f dimer.pdb -o topol.top -x protein_cg.pdb -p backbone -ff martini3001 -cys auto
 
 #FOR MARTINI 3 PROTEIN-WATER SCALED SIMULATION, ONE MUST CHANGE THE EPSILON VALUE FOR LJ POTENTIAL BETWEEN PROTEIN AND WATER ATOMS. THE martini_v3.0.0/martini_v3.0.0.itp FILE CONTAINS THE PROTEIN-WATER VALUES, WHICH CAN BE MODIFIED USING PYTHON FILE INSIDE THE FOLDER "LAMBDA".
-martinize2 -f dimer.pdb -o topol.top -x protein_cg.pdb -p backbone -ff martini3001 -cys auto -merge all
+martinize2 -f dimer.pdb -o topol.top -x protein_cg.pdb -p backbone -ff martini3001 -cys auto
 
 #FOR MARTINI 3 IDP SIMULATION, RUN FOLLOWWING COMMAND
-martinize2 -f dimer.pdb -o topol.top -x protein_cg.pdb -ff martini3IDP -cys auto -idr-tune -id-regions 1:42 -merge all
+martinize2 -f dimer.pdb -o topol.top -x protein_cg.pdb -ff martini3IDP -cys auto -idr-tune -id-regions 1:42
 
 ######################################################
 ##############################################################
 #######################################################################
 
 # UPTO NOW ONE MUST HAVE A TOPOL.TOP FILE AND A COARSE GRAINED PDB FILE, AND AN .ITP FILE READY ACCORDING TO FORCE FIELD SELECTED READY WITHIN THE WORKING DIRECTORY.
-# ONE MUST HAVE A MDP FOLDER AND martini_v3 FOLDER READY TOO. I HAVE INCLUDED THE DEFUALT LAMBDA OF 1 FOR PROTEIN-WATER SCALING IN martini_v3.0.0/martini_v3.0.0.itp, ONE CAN CHANGE IT USING THE LAMBDA DIRECTORY PYTHON FILE.
+# ONE MUST HAVE A MDP FOLDER AND martini_v3 FOLDER READY TOO (CHECK AGAIN THE FOLDER NAME AGAIN, martini_v300 and martini_v3.0.0 are supposed to be samr). I HAVE INCLUDED THE DEFUALT LAMBDA OF 1 FOR PROTEIN-WATER SCALING IN martini_v3.0.0/martini_v3.0.0.itp, ONE CAN CHANGE IT USING THE LAMBDA DIRECTORY PYTHON FILE.
 
-cat > topol.top <<EOF
-#include "martini_v3.0.0/martini_v3.0.0.itp"
-#include "martini_v3.0.0/martini_v3.0.0_ions_v1.itp"
-#include "martini_v3.0.0/martini_v3.0.0_solvents_v1.itp"
-#include "protein0.itp"
-EOF
+
+module load gromacs/2022.2
+
+
+sed -i '
+1,/^$/c\
+#include "martini_v3.0.0/martini_v3.0.0.itp"\
+#include "martini_v3.0.0/martini_v3.0.0_ions_v1.itp"\
+#include "martini_v3.0.0/martini_v3.0.0_solvents_v1.itp"\
+#include "molecule_0.itp"\
+' topol.top
+
 
 #CHANGE THE BOX SIZE ACCORDINGLY
 gmx editconf -f protein_cg.pdb -box 13.6 13.6 13.6 -bt cubic -o protein_cg.gro
@@ -37,7 +45,7 @@ gmx mdrun -v -deffnm em_vac/em_vac
 
 #SOLVATION IN THE BOX 
 mkdir -p sol
-gmx solvate -cp em_vac/em_vac.gro -cs mdp_files/water.gro -radius 0.21 -o sol/sol.gro -p topol.top -maxsol 21000 
+gmx solvate -cp em_vac/em_vac.gro -cs mdp_files/water.gro -radius 0.21 -o sol/sol.gro -p topol.top
 
 #ADDYING IONS IN THE SOLVATED BOX
 gmx grompp -f mdp_files/ions.mdp -c sol/sol.gro -p topol.top -o sol/ions.tpr -maxwarn 1
